98205430e0fe8b5b50db665b27d3eb6db736c8b2
Harshal <harshalrajnor852056@gmail.com>
2021-12-21 17:20:35 +0530

    This File Explains the Variables and User Input in C

---
+++ variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+
+    int a = 15;
+    printf("The value of a is: %d \n", a);
+
+    printf("Enter the number");
+    int b;
+    scanf("%d", &b);
+    printf("The value of b is:%d \n", b);
+
+    int aa = 10;
+    int bb = 11.12;
+    float cf = 4.5;
+    char cc = '$';
+    printf("%d \n", aa);
+    printf("%d \n", bb);
+    printf("%f \n", cf);
+    printf("%c \n", cc);
+
+    printf("The sum of aa & bb is %d \n", aa + bb);
+    return 0;
+}
