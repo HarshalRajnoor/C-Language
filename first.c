e939bb2e1f49934a4e070b513706c8998b1eee47
Harshal <harshalrajnor852056@gmail.com>
2021-12-22 20:23:13 +0530

    This is the basic structure of a C program

--- first.c
+++ first.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main(){
-    printf("Hello, I am learning C with Harry ");
+    printf("Hello, I am learning C with Harry \n");
+    // \n -- Pirnts new line
+    // \t -- prints a tab
 return 0;
 }
