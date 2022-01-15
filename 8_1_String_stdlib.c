69663b8d04a6b66a1a74383e189d7a70bd84d079
Harshal <harshalrajnor852056@gmail.com>
2022-01-15 09:10:39 +0530

    This file explains the functions of string.h library

--- 8_1_String_stdlib.c
+++ 8_1_String_stdlib.c
@@ -2,21 +2,21 @@
 #include <string.h>// provides standard functions for string manipulation
 void main(){
     // strlen -> this function is used to count the length of a string excluding the null character(\0)
-    // char *str = "Harshal Sunil Rajnoor";
-    // int a = strlen(str);
-    // printf("The length of the string is %d \n", a);
+    char *str = "Harshal Sunil Rajnoor";
+    int a = strlen(str);
+    printf("The length of the string is %d \n", a);
 
     // strcpy --> this function is used to copy the content of second string into first string passed to it 
-    // char source[] = "Harshal";
-    // char target[50];
-    // strcpy(target, source);
-    // printf("The characters in target array are %s \n", target);
+    char source[] = "Harshal";
+    char target[50];
+    strcpy(target, source);
+    printf("The characters in target array are %s \n", target);
 
     // strcat --> this function is used to concatenate two strings 
-    // char s1[8] = "Harshal";
-    // char s2[]  = "Rajnoor";
-    // strcat(s1,s2);// s1 contains HarshalRajnoor (no space in between)
-    // printf("The characters in s1 are %s \n", s1);
+    char s1[8] = "Harshal";
+    char s2[]  = "Rajnoor";
+    strcat(s1,s2);// s1 contains HarshalRajnoor (no space in between)
+    printf("The characters in s1 are %s \n", s1);
 
     /* strcmp --> 1) This function is used to compare two strings
                   2) It returns :0 if strings are equal
